57ebbe95700c9bdf41d017bd6048917df27f728b
Medhat Ibrahim Zaki Ahmed <medhat.ibrahimzakiahmed@campus.tu-berlin.de>
2019-01-15 15:45:45 +0100

    Upload New File

---
+++ ml/src/main/scala/org/dma/sketchml/ml/data/LabeledData.scala
@@ -0,0 +1,9 @@
+package org.dma.sketchml.ml.data
+
+import org.apache.flink.ml.math.Vector
+
+
+case class LabeledData(label: Double, feature: Vector) {
+
+  override def toString: String = s"($label $feature)"
+}
