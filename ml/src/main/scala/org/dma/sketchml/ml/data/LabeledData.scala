44a50a3e1870b0c6abaa87f0cd32af667b6c31aa
Medhat Ibrahim Zaki Ahmed <medhat.ibrahimzakiahmed@campus.tu-berlin.de>
2019-01-15 15:44:37 +0100

    Delete LabeledData.scala

--- ml/src/main/scala/org/dma/sketchml/ml/data/LabeledData.scala
+++
@@ -1,8 +0,0 @@
-package org.dma.sketchml.ml.data
-
-import org.apache.flink.ml.math.Vector
-
-case class LabeledData(label: Double, feature: Vector) {
-
-  override def toString: String = s"($label $feature)"
-}
