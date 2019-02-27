d5e9097274523b13f07dbf588a10d8e432b305a4
Jose Alejandro <ja.camino@alumnos.upm.es>
2019-02-27 11:40:56 +0100

    Fix zero gradient

--- ml/src/main/scala/org/dma/sketchml/ml/gradient/SparseDoubleGradient.scala
+++ ml/src/main/scala/org/dma/sketchml/ml/gradient/SparseDoubleGradient.scala
@@ -8,10 +8,12 @@ class SparseDoubleGradient(d: Int, val indices: Array[Int],
   {
     require(indices.length == values.length,
       s"Sizes of indices and values not match: ${indices.length} & ${values.length}")
-    require(indices.head >= 0, s"Negative index: ${indices.head}.")
-    for (i <- 1 until indices.length)
-      require(indices(i - 1) < indices(i), s"Indices are not strictly increasing")
-    require(indices.last < dim, s"Index ${indices.last} out of bounds for gradient of dimension $dim")
+    if(indices.length > 0) {
+      require(indices.head >= 0, s"Negative index: ${indices.head}.")
+      for (i <- 1 until indices.length)
+        require(indices(i - 1) < indices(i), s"Indices are not strictly increasing")
+      require(indices.last < dim, s"Index ${indices.last} out of bounds for gradient of dimension $dim")
+    }
   }
 
   //override def plusBy(sparse: SparseDoubleGradient): Gradient = {
